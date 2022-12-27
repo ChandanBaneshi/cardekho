a2cfdb6a27971fe5c67a33d762370ea708113e4a
chandan baneshi <unkn7666@gmail.com>
2022-12-27 19:54:28 +0530

    car dekho using javascript

---
+++ car.js
@@ -0,0 +1,18 @@
+
+function showred(){
+	document.getElementById("image").innerHTML='<img src=red.jgp>';
+}
+function showgreen(){
+	document.getElementById("image").innerHTML='<img src=>';
+}
+function showblack(){
+	document.getElementById("image").innerHTML='<img src='>;
+}
+function showwhite(){
+	document.getElementById("image").innerHTML='<img src='>;
+}
+
+function showgray(){
+	document.getElementById("image").innerHTML='<img src='>;
+}
+
